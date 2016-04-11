471d9dbff920d376e6683acd3f881a292013bbc8
Anton Pripachkin <anton.pripachkin@extrawest.com>
2016-04-11 13:00:44 +0300

    Initial commit

---
+++ app/src/main/java/apripachkin/com/bucketdrops/MainActivity.java
@@ -0,0 +1,13 @@
+package apripachkin.com.bucketdrops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
