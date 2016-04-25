3f6863b59b6a362ba1b507527dc520f7224671fb
Anton Pripachkin <anton.pripachkin@extrawest.com>
2016-04-25 14:31:36 +0300

    Added missing files

---
+++ app/src/main/java/apripachkin/com/bucketdrops/adapters/AddListener.java
@@ -0,0 +1,6 @@
+package apripachkin.com.bucketdrops.adapters;
+
+
+public interface AddListener {
+    void add();
+}
---
+++ app/src/main/java/apripachkin/com/bucketdrops/extras/Util.java
@@ -0,0 +1,34 @@
+package apripachkin.com.bucketdrops.extras;
+
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.view.View;
+
+import java.util.List;
+
+
+public class Util {
+    public static void showViews(List<View> views) {
+        for (View view : views) {
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public static void hideViews(List<View> views) {
+        for (View view : views) {
+            view.setVisibility(View.GONE);
+        }
+    }
+
+    public static boolean moreThanJellyBean() {
+        return Build.VERSION.SDK_INT > 15;
+    }
+
+    public static void setBackground(View view, Drawable drawable) {
+        if (moreThanJellyBean()) {
+            view.setBackground(drawable);
+        } else {
+            view.setBackgroundDrawable(drawable);
+        }
+    }
+}
